70a1c4a2b76ad987366ef2c4876abe115e1817fc
jxhnkndl <jkroyston@gmail.com>
2024-03-27 10:40:26 -0400

    configure embedded sanity studio route

--- app/admin/[[...index]]/page.tsx
+++ app/admin/[[...index]]/page.tsx
@@ -1,16 +1,10 @@
 'use client'
 
-/**
- * This route is responsible for the built-in authoring environment using Sanity Studio.
- * All routes under your studio path is handled by this file using Next.js' catch-all routes:
- * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
- *
- * You can learn more about the next-sanity package here:
- * https://github.com/sanity-io/next-sanity
- */
+// This route is where the Sanity Studio gets served
+// It has to be a client component because it requires a client interaction
 
 import { NextStudio } from 'next-sanity/studio'
-import config from '../../../sanity.config'
+import config from '@/sanity.config'
 
 export default function StudioPage() {
   return <NextStudio config={config} />
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,16 +1,17 @@
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `/app/admin/[[...index]]/page.tsx` route
- */
+// This configuration is used to for the Sanity Studio that’s mounted on the `/app/admin/[[...index]]/page.tsx` route. 
+
+// The /admin/[[index]]/page.tsx ensures that any route including the admin endpoint redirects to the Sanity Studio
+
 
 import { visionTool } from '@sanity/vision';
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
 
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from './sanity/env';
 import { schema } from './sanity/schema';
 
 export default defineConfig({
+  // This the URL where the embedded Sanity studio will live
   basePath: '/admin',
   projectId,
   dataset,
